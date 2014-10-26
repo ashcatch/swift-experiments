6013f565da076bf3a71ce7c235d5f53af3d90cc4
Rainer Schmid <rms@froglogic.com>
2014-10-26 12:52:29 +0100

    Add a countdown timer in the status bar

--- TeaTime/TeaTime/AppDelegate.swift
+++ TeaTime/TeaTime/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
+    var countDownTimer: NSTimer!
+    var statusItem: NSStatusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1) // -1 is NSVariableStatusItemLength
+    var finishingTime: NSTimeInterval = 0
+
     func applicationDidFinishLaunching(aNotification: NSNotification)
     {
         NSApplication.sharedApplication().hide(nil)
@@ -24,14 +28,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 selector: Selector("teaReady:"),
                 userInfo: nil,
                 repeats: false)
+
+        self.finishingTime = NSDate.timeIntervalSinceReferenceDate() + teaTimeSeconds
+        self.countDownTimer = NSTimer.scheduledTimerWithTimeInterval(1.0,
+            target: self,
+            selector: Selector("countDown:"),
+            userInfo: nil,
+            repeats: true)
+        self.countDown(self.countDownTimer)
     }
 
     func applicationWillTerminate(aNotification: NSNotification)
     {
     }
 
+    func countDown(timer: NSTimer)
+    {
+        let timeLeft = Int(self.finishingTime - NSDate.timeIntervalSinceReferenceDate()) + 1
+        let minutes = timeLeft / 60
+        let seconds = timeLeft % 60
+        self.statusItem.title = "☕️ \(minutes):\(seconds)"
+    }
+
     func teaReady(timer: NSTimer)
     {
+        self.countDownTimer.invalidate()
+        self.statusItem.title = "☕️ fertig!"
+
         NSApplication.sharedApplication().activateIgnoringOtherApps(true)
 
         let alert = NSAlert();
